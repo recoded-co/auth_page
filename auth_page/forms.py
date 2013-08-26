e3a7c60d01cfaf2997e727f92823559432a9f73c
Martut <martut@man.poznan.pl>
2013-08-26 16:11:07 +0200

    authentication forms extended(email or username login, registration use first_name, last_name, email attributes)

--- auth_page/forms.py
+++ auth_page/forms.py
@@ -1,5 +1,13 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django import forms
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.auth.hashers import check_password
+from django.db.models import Q
+
 
 class ProfileForm(ModelForm):
     
@@ -8,3 +16,78 @@ class ProfileForm(ModelForm):
         fields = ('first_name',
                   'last_name',
                   'email')
+        
+class UserCreationFormExtended(UserCreationForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    
+    email = forms.EmailField(label=_("E-mail"))
+    first_name = forms.CharField(label=_("First name"))
+    last_name = forms.CharField(label=_("Last name"))
+    
+    class Meta:
+        model = User
+        fields = ("username","email","first_name","last_name")
+        
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+            
+        if commit:
+            user.save()
+        return user
+    
+class AuthenticationFormExtended(AuthenticationForm):
+    
+    username = forms.CharField(label=_('username/e-mail'),max_length=254)
+    
+    error_messages = {
+        'invalid_login': _("Please enter a correct user name or e-mail and password. "
+                           "Note that both fields may be case-sensitive."),
+        'no_cookies': _("Your Web browser doesn't appear to have cookies "
+                        "enabled. Cookies are required for logging in."),
+        'inactive': _("This account is inactive."),
+    }
+    
+    def is_valid(self):
+        
+        valid = super(AuthenticationFormExtended, self).is_valid()
+        if valid:
+            return True;
+        
+        # so far so good, get this user based on the username or email
+        try:
+            user = User.objects.get(
+                Q(username=self.data['username']) | Q(email=self.data['username'])
+            )
+ 
+        # no user with this username or email address
+        except User.DoesNotExist:
+            self._errors['no_user'] = _('User does not exist')
+            return False
+ 
+        # verify the passwords match
+        if not check_password(self.data['password'], user.password):
+            self._errors['invalid_password'] = _('Password is invalid')
+            return False
+ 
+        # all good
+        return True
+    
+    def get_user(self):
+        
+        if self.is_valid():
+            print "self.is_valid()"
+            user = User.objects.get(
+                Q(username=self.data['username']) | Q(email=self.data['username'])
+                )
+            user.backend='django.contrib.auth.backends.ModelBackend'
+            return user
+        else:
+            raise  User.DoesNotExist()
+    
--- auth_page/views.py
+++ auth_page/views.py
@@ -1,11 +1,9 @@
-from datetime import datetime
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate ,  REDIRECT_FIELD_NAME
 from django.contrib.auth import login as django_login
 from django.contrib.auth import logout as django_logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import login as django_login_view
 from django.contrib.auth.views import password_reset
@@ -16,29 +14,96 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.template.loader import render_to_string
-from hashlib import sha1 as sha_constructor
-from forms import ProfileForm
+from django.utils.hashcompat import sha_constructor
+from forms import ProfileForm, UserCreationFormExtended, AuthenticationFormExtended
 from models import Email
 from random import random
+from django.utils.translation import ugettext, ugettext_lazy as _
+import hashlib
+from datetime import datetime , timedelta
+from django.utils import timezone
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.sites.models import get_current_site
+from django.template.response import TemplateResponse
+from django.utils.http import base36_to_int, is_safe_url
 
-def login(request):
+"""def login(request):
     return django_login_view(request, 'login.html')
+"""
+
+@sensitive_post_parameters()
+@csrf_protect
+@never_cache
+def login(request, template_name='login.html',
+          redirect_field_name=REDIRECT_FIELD_NAME,
+          authentication_form=AuthenticationFormExtended,
+          current_app=None, extra_context=None):
+    """
+    Displays the login form and handles the login action.
+    """
+    redirect_to = request.REQUEST.get(redirect_field_name, '')
+
+    if request.method == "POST":
+        form = authentication_form(data=request.POST)
+        if form.is_valid():
+            # Ensure the user-originating redirection url is safe.
+            """if not is_safe_url(url=redirect_to, host=request.get_host()):
+                redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+            """
+            # Okay, security check complete. Log the user in.
+            django_login(request, form.get_user())
+
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
+
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = authentication_form(request)
+
+    request.session.set_test_cookie()
+
+    current_site = get_current_site(request)
+
+    context = {
+        'form': form,
+        redirect_field_name: redirect_to,
+        'site': current_site,
+        'site_name': current_site.name,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context,
+                            current_app=current_app)
+
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationFormExtended(request.POST)
         if form.is_valid():
-            User.objects.create_user(form.cleaned_data['username'],
-                                    '',
+            
+            user = User.objects.create_user(form.cleaned_data['username'],
+                                    form.cleaned_data['email'],
                                     password=form.cleaned_data['password1'])
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.save()
             
             #form.save()
+            print str(user)
             
-            new_user = authenticate(username = request.POST.get('username'),
-                                    password = request.POST.get('password1'))
-            django_login(request, new_user)
+            new_user = authenticate(username = request.POST.get('username'), password = request.POST.get('password1'))
+            #django_login(request, new_user)
+            send_registration_confirmation(new_user)
             
-            return HttpResponseRedirect(request.POST.get('next', '/'))
+            next = request.POST.get('next', '/')
+            return render_to_response('account_confirmed.html',
+                                      { 'next': next},
+                                        context_instance = RequestContext(request))
+            
+            #return HttpResponseRedirect(request.POST.get('next', '/'))
         else:            
             next = request.POST.get('next', '/')
             return render_to_response('register.html',
@@ -46,13 +111,56 @@ def register(request):
                                         'next': next},
                                         context_instance = RequestContext(request))
     else:
-        form = UserCreationForm()
+        form = UserCreationFormExtended()
         next = request.GET.get('next', '/')
         return render_to_response('register.html',
                                   {'form': form,
                                     'next': next},
                                     context_instance = RequestContext(request))
+  
+def send_registration_confirmation(user):
+    title = _("Account confirmation")
     
+    current_site = Site.objects.get_current()
+                    
+    activate_url = u"http://%s%s%s%s%s" % (unicode(current_site.domain),
+                                             '/auth_page/confirm_registration/',
+                                             str(confirmation_code(user)),
+                                             '/',
+                                             user.username)
+    
+    content = "%s %s %s %s %s" % ( _('Welcome'),
+                                   user.first_name,
+                                   user.last_name,
+                                   _('pleas confirm your account by url:'),
+                                   activate_url)
+    send_mail(title, content, 'marcinradon@recoded.co', [user.email], fail_silently=False)
+
+def confirmation_code(user):
+    h = hashlib.md5();
+    h.update(user.username)
+    h.update('confirm')
+    h.update(user.email)
+    print h.hexdigest()
+    return h.hexdigest()
+
+def confirm_registration(request, code, username):
+    try:
+        user = User.objects.get(username=username)
+        print datetime.now()
+        print timedelta(days=1)
+        print (datetime.now()-timedelta(days=1))
+        
+        if confirmation_code(user) == code and user.date_joined > timezone.make_aware((datetime.now()-timedelta(days=1)), timezone.get_default_timezone()):
+            user.is_active = True
+            user.save()
+            user.backend='django.contrib.auth.backends.ModelBackend' 
+            django_login(request, user)
+            return HttpResponseRedirect(request.POST.get('next', '/'))
+    except Exception as e:
+        print e
+        return HttpResponseRedirect(request.POST.get('next', '/'))
+
 
 def logout(request):
     django_logout(request)
@@ -121,6 +229,7 @@ def profile(request):
                                   context_instance = RequestContext(request))
         
 def confirm_email(request, confirmation_key):
+    print "confirm_email"
     try:
         email = Email.objects.get(confirmation_key = confirmation_key,
                                   email_is_verified = False)
